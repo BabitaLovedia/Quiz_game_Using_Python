677160a8f9ef7d844864d08aeac1d4d4c2c1e7fc
BabitaLovedia <babitalovedia7@gmail.com>
2024-07-08 19:54:28 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,46 @@
+quiz_data = [
+    {
+        "question": "what is the capital of France?",
+        "choices":  ["Paris" , "London", "Berlin" , "Madrid"],
+        "answer": "Paris"
+    },
+    { 
+        "question": "what is the largest planet  in our solar sysrem?",
+        "choices":["jupiter" , "Saturn", "Mars" , "Earth" ],
+        "answer": "jupiter"
+    },
+    {
+        "question": "what is the  chemical symbol for Gold?",
+        "choices":["Go" , "Au", "Gd" , "Ag"],
+        "answer": "Au"
+    },
+    {   "question": "Which country is known as the 'land of the rising sun'?",
+        "choices":["japan" , "china", "Thailand" , "South Korea" ],
+        "answer": "japan"
+    },
+    {   "question": "what is the name of the weak zone of the earth's crust?",
+        "choices":["cosmic" , "seismic", "formic","ozone" ],
+        "answer": "seismic"
+    },
+    {   "question": "which of the disease is caused by protozoa?",
+        "choices":["cancer" , "kala-azar", "typhoid" ],
+        "answer": "kala-azar"   
+    },
+    {   "question": "when the metal react with dilute acid , which gas is formed?",
+        "choices":["hydrpgen" , "helium", "neon" ],
+        "answer": "hydrogen"
+    },
+    {   "question": " which of the given vitamin is a water-soluble vitamin?",
+        "choices":["Vitamin A" , "Vitamin B", "Vitamin C" , "Vitamin D"],
+        "answer":"Vitamin B"
+    },
+    {   "question": "Salt is obtained from sea water through which process?",
+        "choices":["Evaporation" , "Absorption", "Sublimation" , "Precipitation"],
+        "answer": "Sublimation"
+    },
+    {   "question": "where was the first winter olympics held?",
+        "choices":["Greece" , "France", "Germany" , "China"],
+        "answer": "Greece"
+    }
+
+]
